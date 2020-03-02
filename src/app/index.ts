a879cfb8fa1e90f544bb368f8206b0b25fa804de
Sun Yongzhen <1315976441@qq.com>
2020-03-02 16:15:03 +0800

    init webpack

---
+++ src/app/index.ts
@@ -0,0 +1,5 @@
+function index(a,b): number {
+  return a + b;
+}
+const test_index = index(1,2);
+console.log(test_index);
---
+++ src/app/picture.ts
@@ -0,0 +1,5 @@
+function picture(a,b) {
+  return a + b;
+}
+const test_picture = picture(1,2);
+console.log(test_picture);
