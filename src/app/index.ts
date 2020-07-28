c48079118603e1b81fc148daed918384ff43aa13
Sun Yongzhen <1315976441@qq.com>
2020-07-28 15:50:38 +0800

    update decorator

--- src/app/index.ts
+++ src/app/index.ts
@@ -8,20 +8,20 @@ import gql from 'graphql-tag';
 
 $('#btn').bind('click', function() {
 
-  const client = new ApolloClient({
-    uri: 'http://localhost:4000'
-  });
-  client.query({
-    query: gql`
-      query TodoApp {
-        books {
-          title
-        }
-      }
-    `,
-  })
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+  // const client = new ApolloClient({
+  //   uri: 'http://localhost:4000'
+  // });
+  // client.query({
+  //   query: gql`
+  //     query TodoApp {
+  //       books {
+  //         title
+  //       }
+  //     }
+  //   `,
+  // })
+  //   .then(data => console.log(data))
+  //   .catch(error => console.error(error));
 
 
   // const url = 'http://localhost:8082/json2xlsx';
@@ -91,6 +91,41 @@ $('#btn').bind('click', function() {
   //     }
   //   }
   // });
+
+
+  class Somebody {
+    speed: number = 10;
+    name: string;
+  
+    constructor(name: string) {
+      this.name = name;
+    }
+  
+    hit(rival: Somebody) {
+      const hitDamage: number = 10;
+      console.log(`${this.name}对${rival.name}造成一次伤害：${hitDamage}`);
+    }
+  }
+  
+  function cheating(target: any) {
+    target.prototype.hit = function(rival: Somebody) {
+      const hitDamage: number = 100;
+      console.log(`${this.name}对${rival.name}造成一次伤害: ${hitDamage}`);
+    };
+  }
+  
+  @cheating
+  class SBody extends Somebody{
+  
+  }
+  
+  const s0 = new Somebody('小红0');
+  const s1 = new SBody('小红1');
+  const rival = new Somebody('小明');
+  s0.hit(rival);
+  s1.hit(rival);
+
+
 });
 
 
